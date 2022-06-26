c5fd461a563d529596e4e0fea1325e1d22d9dc2e
owensnyder <owsnyder@ncsu.edu>
2022-06-26 15:52:49 -0400

    RENDER CODE

--- renderCode.R
+++ renderCode.R
@@ -1,6 +1,5 @@
 rmarkdown::render('README.md',
                   output_format = "github_document",
-                  #output_dir = "_posts",
                   output_options = list(
                     html_preview = FALSE, toc = TRUE, toc_depth = 2, toc_float = TRUE)
 )
