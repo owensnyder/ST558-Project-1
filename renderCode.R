31cb300ad4be858fd5be89d944372cb219f499fd
owensnyder <owsnyder@ncsu.edu>
2022-06-26 22:38:30 -0400

    FINAL RENDER CODE

--- renderCode.R
+++ renderCode.R
@@ -1,6 +1,6 @@
-rmarkdown::render('README.md',
+rmarkdown::render("st558 Project1 rmd.Rmd",
                   output_format = "github_document",
+                  output_file = "README.md",
                   output_options = list(
                     html_preview = FALSE, toc = TRUE, toc_depth = 2, toc_float = TRUE)
 )
-
